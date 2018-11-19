2d80bcf225b470771ca68f3fd02b7a420aaa7530
Eadral <a250014238@me.com>
2018-11-19 22:51:43 +0800

    change

---
+++ Assets/Scripts/Tutorial/Tank/TankMovementT.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMovementT : MonoBehaviour {
+    public int m_PlayerNumber = 1;
+    public float m_Speed = 12f;
+    public float m_TurnSpeed = 180f;
+    public AudioSource m_MovementAudio;
+    public AudioClip m_EngineIdling;
+    public AudioClip m_EngineDriving;
+    public float m_PitchRange = 0.2f;
+
+    private string m_MovementAxisName;
+    private string m_TurnAxisName;
+    private Rigidbody m_Rigidbody;
+    private float m_MovementInputValue;
+    private float m_TurnInputValue;
+    private float m_OriginalPitch;
+    private ParticleSystem[] m_particleSystems;
+    // Use this for initialization
+
+
+    private void Awake() {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Start () {
+        m_MovementAxisName = "Vertical" + m_PlayerNumber;
+        m_TurnAxisName = "Horizontal" + m_PlayerNumber;
+        
+
+    }
+
+    private void FixedUpdate() {
+
+        Move();
+        Turn();
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
+        m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
+	}
+
+    void Move() {
+        Vector3 movement = new Vector3();
+        movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
+        m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+    }
+
+    void Turn() {
+        float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
+        Quaternion turnRotation = Quaternion.Euler(0, turn, 0);
+        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * turnRotation);
+    }
+
+
+
+
+
+
+}
